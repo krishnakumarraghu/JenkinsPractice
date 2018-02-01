9555ad49757207bc7929767842ebd156e4f313de
Krishna <krshnkmr@gmail.com>
2018-02-01 16:37:49 +0530

    Update Vets.java
    Renamed field "vets"

--- src/main/java/org/springframework/samples/petclinic/model/Vets.java
+++ src/main/java/org/springframework/samples/petclinic/model/Vets.java
@@ -30,14 +30,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Vets {
 
-    private List<Vet> vets;
+    private List<Vet> lvets;
 
     @XmlElement
     public List<Vet> getVetList() {
-        if (vets == null) {
-            vets = new ArrayList<>();
+        if (lvets == null) {
+            lvets = new ArrayList<>();
         }
-        return vets;
+        return lvets;
     }
 
 }
